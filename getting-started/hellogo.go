7da0daea87063aa7e1332a61d7f4935113ee05eb
chevonmdphillip <chevon@chevonphillip.com>
2020-06-09 12:11:31 -0400

    Working with pointers

--- getting-started/hellogo.go
+++ getting-started/hellogo.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from golang")
+	greeting := "Hello Go!"
+	fmt.Println(greeting)
+
+	prt := &greeting
+	fmt.Println(prt, *prt)
+
+	greeting = "Hello world from Go!"
+	fmt.Println(prt, *prt)
 }
